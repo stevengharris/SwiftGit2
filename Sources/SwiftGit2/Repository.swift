049d6f8b1706842cb2b287eabe9b32fb0ffed08f
Steven G. Harris <steven.g.harris@gmail.com>
2026-04-16 15:59:12 -0700

    Restore mods to handle the initial repo commit (i.e., when parent is nil).

--- Sources/SwiftGit2/Repository.swift
+++ Sources/SwiftGit2/Repository.swift
@@ -692,10 +692,10 @@ public final class Repository {
 				return .failure(err)
 			}
 			var parentID = git_oid()
-			//if git_oid_is_zero(&parentID) == 1 {
-            //    // The parent is nil, so just commit the tree without parentCommits
-            //    return commit(tree: OID(treeOID), parents: [], message: message, signature: signature)
-            //} else {
+			if git_oid_is_zero(&parentID) == 1 {
+                // The parent is nil, so just commit the tree without parentCommits
+                return commit(tree: OID(treeOID), parents: [], message: message, signature: signature)
+            } else {
                 let nameToIDResult = git_reference_name_to_id(&parentID, self.pointer, "HEAD")
                 guard nameToIDResult == GIT_OK.rawValue else {
                     return .failure(NSError(gitError: nameToIDResult, pointOfFailure: "git_reference_name_to_id"))
@@ -703,7 +703,7 @@ public final class Repository {
                 return commit(OID(parentID)).flatMap { parentCommit in
                     commit(tree: OID(treeOID), parents: [parentCommit], message: message, signature: signature)
                 }
-            //}
+            }
 		}
 	}
 
